04fd60b2e90ecc5b58fcded91af1099b4805ffef
emmachilds98-wq <emma.childs98@gmail.com>
2026-08-02 21:46:45 +0100

    Restore service-worker.js and bump to v295

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,47 @@
-PLACEHOLDER
+// Bump CACHE_VERSION any time you publish an update to force refresh of cached assets.
+const CACHE_VERSION = "v295";
+const CACHE_NAME = `boomtown-companion-${CACHE_VERSION}`;
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./js/artist-bios.js",
+  "./js/artist-previews.js",
+  "./js/boomtown-locations-2026.js",
+  "./js/pwa-register.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetched = fetch(event.request).then((response) => {
+        if (response && response.status === 200 && response.type === "basic") {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
